37bd8315f63c569f478a61e5ce1cd5d0486e0d20
Giancrafter <gian.klug@ict-scouts.ch>
2020-04-13 11:41:59 +0200

    new profile page

--- profile.php
+++ profile.php
@@ -12,104 +12,30 @@ if (mysqli_connect_errno()) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
-//Bild wechseln
-if (isset($_POST['changeimage'])) {
+//UPDATE
 
-$stmt = $con->prepare('UPDATE accounts SET profile_pic = ? WHERE id = ?');
-$stmt->bind_param('ss', $_POST['changeimage'], $_SESSION['id']);
-$stmt->execute();
-$stmt->close();
-
-$alert = '<div class="alert alert-success" role="alert">
-Profilbild erfolgreich geändert!
-</div>';
-}
-
-
-//Name ändern
 if (isset($_POST['changename'])) {
-	if ($stmtu = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
-		// Bind parameters (s = string, i = int, b = blob, etc), hash the password using the PHP password_hash function.
-		$stmtu->bind_param('s', $_POST['changename']);
-		$stmtu->execute();
-		$stmtu->store_result();
-		// Store the result so we can check if the account exists in the database.
-		if ($stmtu->num_rows > 0) {
-			// Username already exists
-	    $alert = '<div class="alert alert-danger" role="alert">
+if ($stmtu = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+$stmtu->bind_param('s', $_POST['changename']);
+$stmtu->execute();
+$stmtu->store_result();
+
+if ($stmtu->num_rows > 0) {
+    // Username already exists
+    $alert = '<div class="alert alert-danger" role="alert">
 	Benutzername existiert bereits
 	</div>';} else {
-$stmt = $con->prepare('UPDATE accounts SET username = ? WHERE id = ?');
-$stmt->bind_param('ss', $_POST['changename'], $_SESSION['id']);
-$_SESSION['name'] = $_POST['changename'];
-$stmt->execute();
-$stmt->close();
-$alert = '<div class="alert alert-success" role="alert">
-Name erfolgreich geändert!
-</div>';
-}}}
-
-
-//geschlecht+interesse
-if (isset($_POST['changegender'])) {
-
-$stmt = $con->prepare('UPDATE accounts SET gender = ?, search = ? WHERE id = ?');
-$stmt->bind_param('iii', $_POST['changegender'],  $_POST['changegender2'], $_SESSION['id']);
+$stmt = $con->prepare('UPDATE accounts SET username = ?, gender = ?, search = ?, description = ?, age = ? location = ? WHERE id = ?');
+$stmt->bind_param('siissiii', $_POST['changename'], $_POST['changegender'],  $_POST['changegender2'], $_POST['changedescription'], $_POST['changeage'], $_POST['changelocation'], $_SESSION['id']);
 $stmt->execute();
 $stmt->close();
 
 $alert = '<div class="alert alert-success" role="alert">
-Geschlecht/Interesse erfolgreich geändert.
-</div>';
-}
-if (isset($_POST['changedescription'])) {
-
-$stmt = $con->prepare('UPDATE accounts SET description = ? WHERE id = ?');
-$stmt->bind_param('si', $_POST['changedescription'], $_SESSION['id']);
-$stmt->execute();
-$stmt->close();
-
-$alert = '<div class="alert alert-success" role="alert">
-Beschreibung erfolgreich geändert.
+Profil erfolgreich bearbeitet.
 </div>';
 }
 
 
-
-//Alter
-if (isset($_POST['changeage'])) {
-$stmt = $con->prepare('UPDATE accounts SET age = ? WHERE id = ?');
-$stmt->bind_param('ii', $_POST['changeage'], $_SESSION['id']);
-$stmt->execute();
-$stmt->close();
-
-$alert = '<div class="alert alert-success" role="alert">
-Alter erfolgreich geändert.
-</div>';
-}
-
-
-
-
-//Fragebogen
-if ($_SESSION['fragebogen']==1){$_SESSION['fragebogen']=0; $alert = '<div class="alert alert-success" role="alert">
-Uwo-Hash erfolgreich geändert.
-</div>';}
-
-
-
-//plz
-if (isset($_POST['changelocation'])) {
-
-$stmt = $con->prepare('UPDATE accounts SET location = ? WHERE id = ?');
-$stmt->bind_param('ii', $_POST['changelocation'], $_SESSION['id']);
-$stmt->execute();
-$stmt->close();
-
-$alert = '<div class="alert alert-success" role="alert">
-Alter erfolgreich geändert.
-</div>';
-}
 $stmt = $con->prepare('SELECT password, email, gender, search, profile_pic, description, hash, age, location FROM accounts WHERE id = ?');
 // In this case we can use the account ID to get the account info.
 $stmt->bind_param('i', $_SESSION['id']);
@@ -130,52 +56,31 @@ $stmt->execute();
 $stmt->bind_result($locname, $canton);
 $stmt->fetch();
 $stmt->close();
-$content= $alert . '  <form method="get" style="margin-bottom: 30px;"><input type="text" style="display:none;" name="changeimage" value="1"/><img height="100px" width="100px" src="'.$profile_pic.'" /><br /><input type="submit" class="btn btn-primary d-flex align-items-end" type="button"value="Ändern"></input></form>
-	<form method="get" style="margin-bottom: 30px;">
-		<input type="text" style="display:none;" name="changename" value="1"/>
-			<p><strong>Benutzername:</strong><br>'.$_SESSION['name'].'<input type="submit" class="btn btn-primary d-flex align-items-end" type="button"value="Ändern"></input></p>
-	</form>
-	<form method="get" style="margin-bottom: 30px;">
-	<input type="text" style="display:none;" name="changegender" value="1"/>
-			<p><strong>Geschlecht:</strong><br>'.$genders.'<br><strong>Ich suche:</strong><br>'.$searchs.'<br><input type="submit" class="btn btn-primary d-flex align-items-end" type="button"value="Ändern"></input></p>
-	</form>
-	<form method="get">
-	<input type="text" style="display:none;" name="changedescription" value="1"/>
-			<p><strong>Profilbeschreibung:</strong><br>'.$description.'<input type="submit" class="btn btn-primary d-flex align-items-end" type="button"value="Ändern"></input><br></p>
-	</form>
-	<form method="get" style="margin-bottom: 30px;">
-	<input type="text" style="display:none;" name="fragebogen" value="1"/>
-			<p><strong>Meine Interessen</strong><br>Dein Uwo-Hash:'.$hash.'<input type="submit" class="btn btn-primary d-flex align-items-end" type="button"value="Ändern"></input><br></p>
-	</form>
-	<form method="get" style="margin-bottom: 30px;">
-	<input type="text" style="display:none;" name="changeage" value="1"/>
-			<p><strong>Alter:</strong><br>'.$age.' Jahre<input type="submit" class="btn btn-primary d-flex align-items-end" type="button"value="Ändern"></input></p>
-	</form>
-	<form method="get" style="margin-bottom: 30px;">
-	<input type="text" style="display:none;" name="changelocation" value="1"/>
-			<p><strong>Wohnort:</strong><br>'.$location.' '.$locname.', '.$canton.'<input type="submit" class="btn btn-primary d-flex align-items-end" type="button"value="Ändern"></input></p>
+$content= $alert . '<form method="get" style="margin-bottom: 30px;"><input type="text" style="display:none;" name="edit" value="1"/><img height="100px" width="100px" src="'.$profile_pic.'" /><br />
+<p>'.$_SESSION['name'].'</p>
+<p><i>'.$description.'</i></p>
+<p><strong>Alter:</strong>'.$age.' Jahre</p>
+<p><strong>Ist:</strong>'.$genders.'<br><strong>Sucht:</strong>'.$searchs.'<br></p>
+<p><strong>Wohnort:</strong>'.$location.' '.$locname.', '.$canton.'<br><br><input type="submit" class="btn btn-primary d-flex align-items-end" type="button"value="Profil bearbeiten"></p>
 	</form>';
-// We don't have the passwoord or email info stored in sessions so instead we can get the results from the database.
-if (isset($_GET['changeimage'])) {
-$content = '<form action="profile.php" method="post">
-		<p><strong>Profilbild:</strong><br><input type="text" name="changeimage" "value="'.$profile_pic.'"></input><br /><input type="submit" class="btn btn-primary d-flex align-items-end" type="button"value="Ändern"></input><br></p>
-</form>';
-}
-if (isset($_GET['changename'])) {
-$content = '<form action="profile.php" method="post">
-		<p><strong>Name:</strong><br><input type="text" name="changename" value="' . $name. '"></input><br /><input type="submit" class="btn btn-primary d-flex align-items-end" type="button"value="Ändern"></input><br></p>
-</form>';
-}
-if (isset($_GET['changegender'])) {
-if ($gender==1) {$t1='checked';}else{$t1='';}
-if ($gender==2) {$t2='checked';}else{$t2='';}
-if ($gender==3) {$t3='checked';}else{$t3='';}
-if ($search==1) {$t4='checked';}else{$t4='';}
-if ($search==2) {$t5='checked';}else{$t5='';}
-if ($search==3) {$t6='checked';}else{$t6='';}
-$content = '<form action="profile.php" method="post">
-		<p><strong>Geschlecht:</strong><br>
-		<div class="form-check">
+
+
+if (isset($_GET['edit'])) {
+    if ($gender==1) {$t1='checked';}else{$t1='';}
+    if ($gender==2) {$t2='checked';}else{$t2='';}
+    if ($gender==3) {$t3='checked';}else{$t3='';}
+    if ($search==1) {$t4='checked';}else{$t4='';}
+    if ($search==2) {$t5='checked';}else{$t5='';}
+    if ($search==3) {$t6='checked';}else{$t6='';}
+$content = '<<form action="profile.php" method="post">
+    
+<p><strong>Profilbild: </strong><input class="form-control" type="text" name="changeimage" value="'.$profile_pic.'" /></p>
+<p><strong>Benutzername: </strong><input class="form-control" type="text" name="changename" value="'.$_SESSION['name'].'" /></p>
+<p><strong>Beschreibung: </strong><input class="form-control" type="text" name="changedescription" value="'.$description.'" /></p>
+<p><strong>Alter: </strong><input class="form-control" type="number" name="changeage" value="'.$age.'" min="12" max="20" required /></p>
+
+<p><strong>Geschlecht:</strong><br>
+<div class="form-check">
   <input class="form-check-input" type="radio" name="changegender" id="g1" value="1" '.$t1.'>
   <label class="form-check-label" for="g1">
     Männlich
@@ -212,28 +117,11 @@ $content = '<form action="profile.php" method="post">
     Egal
   </label>
 </div><br />
-		<input type="submit" class="btn btn-primary d-flex align-items-end" type="button"value="Ändern"></input><br></p>
-</form>';
+<p><strong>PLZ: </strong><input class="form-control" type="number" name="changelocation" value="'.$age.'" min="1000" max="9999" required /></p>
+    <input type="submit" class="btn btn-primary d-flex align-items-end" type="button"value="Speichern">
+	</form>';
 }
-if (isset($_GET['changedescription'])) {
-$content = '<form action="profile.php" method="post">
-		<p><strong>Beschreibung:</strong><br><input type="text" name="changeimage" value="'.$description.'"></input><br /><input type="submit" class="btn btn-primary d-flex align-items-end" type="button"value="Ändern"></input><br></p>
-</form>';
-}
-if (isset($_GET['fragebogen'])) {
-	header("Location: fragebogen.php");
-	die();
-}
-if (isset($_GET['changeage'])) {
-$content = '<form action="profile.php" method="post">
-		<p><strong>Alter:</strong><br><input type="text" name="changeage" value="'.$age.'"></input><br /><input type="submit" class="btn btn-primary d-flex align-items-end" type="button"value="Ändern"></input><br></p>
-</form>';
-}
-if (isset($_GET['changelocation'])) {
-$content = '<form action="profile.php" method="post">
-		<p><strong>PLZ:</strong><br><input type="text" name="changelocation" value="'.$location.'"></input><br /><input type="submit" class="btn btn-primary d-flex align-items-end" type="button"value="Ändern"></input><br></p>
-</form>';
-}
+
 ?>
 <!DOCTYPE html>
 <html>
